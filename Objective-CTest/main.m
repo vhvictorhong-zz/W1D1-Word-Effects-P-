7d5c5f82bd857d30f4e0a0852b1f0e239c54b74a
victorhong1988 <victorhong1988@gmail.com>
2016-10-31 17:09:12 -0400

    Initial Commit

---
+++ Objective-CTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-CTest
+//
+//  Created by Victor Hong on 31/10/2016.
+//  Copyright © 2016 Victor Hong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
